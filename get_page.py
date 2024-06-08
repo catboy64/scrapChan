bd8e78050c7999d25252a1aabe53ddccdc5f2756
catboy64 <153577622+catboy64@users.noreply.github.com>
2024-06-08 12:21:23 -0400

    Update get_page.py
    ma fete dit moi bonne fete sinon fuck you

--- get_page.py
+++ get_page.py
@@ -1,8 +1,10 @@
 import color, os
+from bs4 import BeautifulSoup
 
 
 #parsing
 def get_page(soup):
+
     i = 0
     board = soup.find("div", class_="board")
     threads = board.find_all("div", class_="thread")
@@ -26,16 +28,24 @@ def get_page(soup):
         else:
             print(color.colors.BLUE + "**no title**" + color.colors.END)
 
+        blockquote = str(blockquote).replace("</br>", "\n")
+        blockquote = str(blockquote).replace("<br/>", "\n")
+        blockquote = BeautifulSoup(blockquote, "html.parser")
         print(blockquote.text.strip())
         print()
         print()
 
         for reply in postContainerReplys:
+            
             print(color.colors.GREEN + "Anon" + color.colors.END)
             postReply = reply.find("div", class_="post reply")
             replyMessage = postReply.find("blockquote", class_="postMessage")
-            print(replyMessage.text)
+            
+            replyMessage = str(replyMessage).replace("</br>", "\n")
+            replyMessage = str(replyMessage).replace("<br/>", "\n")
+            replyMessage = BeautifulSoup(replyMessage, "html.parser")
+            print(replyMessage.text.strip())
             print()
 
-        print(str(i) + "/15")
+        print(str(i) + "/"+ str(len(threads)))
         input()
